53fb4915f44eec54b85a7bad64ead5fbe83786fc
JaredBrown15 <jared.brown.15@outlook.com>
2025-11-13 18:46:11 -0500

    first commit. Functionality is complete excluding appearance/display and the GUI component

---
+++ flask_api_test.py
@@ -0,0 +1,89 @@
+# using flask_restful
+from flask import Flask, jsonify, request, render_template
+from flask_restful import Resource, Api
+import wala_run
+from flask_cors import CORS
+import threading
+import uuid
+
+
+# creating the flask app
+app = Flask(__name__)
+# creating an API object
+api = Api(app)
+# CORS(app, origins=["https://jaredwjbrown.com", "https://www.jaredwjbrown.com"])
+CORS(app)
+# CORS(app, resources={r"/api/*": {"origins": ["https://jaredwjbrown.com", "https://www.jaredwjbrown.com"]}})
+
+
+jobs = {}
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/submission')
+def submission():
+    return render_template('submission.html')
+
+@app.route('/model_info')
+def model_info():
+    return render_template('model_info.html')
+
+@app.route('/my_info')
+def my_info():
+    return render_template('my_info.html')
+
+
+
+# Background worker function
+def run_wala_task(user_input, job_id):
+    result = wala_run.main(user_input) 
+    jobs[job_id] = {"status": "done", "result": result}
+
+    # if callback:
+    #     # Optional: you could POST to a callback URL or write to a file
+    #     callback(result)
+    # else:
+    #     # Save result to file or database for client to fetch later
+    #     filename = result.get('filename', 'model.obj')
+    #     with open(f"./outputs/{filename}", "w") as f:
+    #         f.write(result['data'])  # assuming base64 string
+
+
+
+# another resource to calculate the square of a number
+class WaLa(Resource):
+
+    def post(self):
+        data = request.get_json()
+        user_input = data.get('prompt')
+        # payload = wala_run.main(user_input)
+        # return jsonify(payload)
+        # Create unique job ID
+        job_id = str(uuid.uuid4())
+        jobs[job_id] = {"status": "pending", "result": None}
+
+        thread = threading.Thread(target=run_wala_task, args=(user_input, job_id))
+        thread.start()
+
+        # Immediately return 202 Accepted
+        # return jsonify({"status": "accepted", "job_id": job_id}), 202
+        return ({"status": "accepted", "job_id": job_id}), 202
+
+api.add_resource(WaLa, '/api')
+
+# Polling endpoint to get JSON result
+@app.route("/api/result/<job_id>")
+def get_result(job_id):
+    job = jobs.get(job_id)
+    if not job:
+        return jsonify({"error": "Job ID not found"}), 404
+    if job["status"] != "done":
+        return jsonify({"status": job["status"]}), 202
+    return jsonify({"status": "done", "result": job["result"]})
+
+
+# driver function
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8000)
---
+++ wala_run.py
@@ -0,0 +1,95 @@
+import os
+import subprocess
+import base64
+
+wala_home = ("/home/jared/documents/phd/distributed_computing/WaLa/")
+gen_home = wala_home + "examples/text/"
+conda_home = '/home/jared/miniconda3/bin/conda'
+
+
+terminal_messages = [
+    "Test",
+    "GENERATING DEPTH MAPS...",
+    "RENAMING FILES...",
+    "GENERATING 3D MODEL"
+]
+# global commands
+commands = [
+    ['mv', gen_home + 'depth_maps/image_0.png', gen_home + 'depth_maps/3.png'],
+    ['mv', gen_home + 'depth_maps/image_1.png', gen_home + 'depth_maps/6.png'],
+    ['mv', gen_home + 'depth_maps/image_2.png', gen_home + 'depth_maps/10.png'],
+    ['mv', gen_home + 'depth_maps/image_3.png', gen_home + 'depth_maps/26.png'],
+    ['mv', gen_home + 'depth_maps/image_4.png', gen_home + 'depth_maps/49.png'],
+    ['mv', gen_home + 'depth_maps/image_5.png', gen_home + 'depth_maps/50.png'],
+
+    [conda_home, 'run', '-n', 'wala', 
+    'python3', wala_home + 'run.py', 
+        '--model_name', 'ADSKAILab/WaLa-DM6-1B', 
+        '--dm6', 
+        gen_home + 'depth_maps/3.png',
+        gen_home + 'depth_maps/6.png',
+        gen_home + 'depth_maps/10.png',
+        gen_home + 'depth_maps/26.png',
+        gen_home + 'depth_maps/49.png',
+        gen_home + 'depth_maps/50.png', 
+        '--output_dir', gen_home,
+        '--output_format', 'obj',
+        '--scale', '1.5',
+        '--diffusion_rescale_timestep', '10']
+]
+
+def serialize_model(path_to_model):
+    # path_to_model = gen_home + '3/3.obj'
+    with open(path_to_model, "rb") as f:
+        obj_bytes = f.read()
+        obj_b64 = base64.b64encode(obj_bytes).decode('utf-8') # Encode to base64 string for safe JSON transport
+    encoded_size = len(obj_b64.encode("utf-8"))
+    print(f"Encoded size: {encoded_size} bytes")
+    payload = {
+        "filename": "3.obj",
+        "data": obj_b64
+    }
+    print("MODEL SUCCESSFULLY ENCODED!!!\n\n\n")
+    return payload
+
+def generate_model(prompt):
+    # global commands
+    gen_cmd = [conda_home, 'run', '-n', 'wala', 
+                'python3', wala_home + 'run.py', 
+                '--model_name', 'ADSKAILab/WaLa-MVDream-DM6', 
+                '--text_to_dm6', f'\"{prompt}\"', 
+                '--output_dir', gen_home]
+    commands.insert(0, gen_cmd)
+    print(f"COMMANDS LENGTH: {len(commands)}\n\n")
+    for i in range(len(commands)):
+        # Run each command in WSL, in the target directory
+        # cmd = ['wsl'] + commands[i]
+        cmd = commands[i]
+        # print(terminal_messages[i])
+        # print(cmd)
+        print(f"Running: {cmd}\n\n")
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True,
+            encoding = 'utf-8'
+        )
+        print(result.stdout)
+        print("STDERR:", result.stderr)
+    path_to_model = '/home/jared/documents/phd/distributed_computing/WaLa/examples/text/3/3.obj'
+    print("FINISHED GENERATING MODEL TO: " + path_to_model)
+    # commands = commands[1:]
+    commands.pop(0)
+    return path_to_model
+
+
+def main(prompt):
+    path_to_model = generate_model(prompt)
+    
+    # path_to_model = gen_home + '3/3.obj'
+#    path_to_model = r'\\wsl.localhost\Ubuntu\home\jared\documents\phd\distributed_computing\WaLa\examples\text\3\3.obj'
+    path_to_model = '/home/jared/documents/phd/distributed_computing/WaLa/examples/text/3/3.obj'
+    payload = serialize_model(path_to_model)
+    return payload
+
+# main("prompt")
